90b87b207e126bd19998bc9c753092be16267923
klamt1ne <klamtine@gmail.com>
2014-09-29 23:15:55 +0800

    add angular todo

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes');
-var users = require('./routes/user');
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ module.exports = function (app) {
 	app.get('/', function (req, res) {
 		res.render('index', { title: 'Express' });
 	})
+
+	app.get('/angular', function (req, res) {
+		res.render('angular', {title: 'angular'})
+	})
 }
